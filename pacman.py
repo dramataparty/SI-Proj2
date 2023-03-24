50b23027292ad51132b9f11389a387af00828690
Dramata <diogobailaoforte@gmail.com>
2023-03-24 10:08:14 +0000

    wefw

--- pacman.py
+++ pacman.py
@@ -1,1 +1,7 @@
 #Escrever aqui o ficheiro .py a entregar em conjunto com a submissao no questionario
+from searchPlus import *
+from p2_aux import *
+
+
+def planeia_online(pacman, pastilha, obstaculos):
+    pass
