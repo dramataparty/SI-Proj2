4b08cd891d8fc67f6866ec3b5db70bf9363b4e10
Dramata <diogobailaoforte@gmail.com>
2023-03-24 10:59:21 +0000

    wrg

--- pacman.py
+++ pacman.py
@@ -1,4 +1,6 @@
 #Escrever aqui o ficheiro .py a entregar em conjunto com a submissao no questionario
+from searchPlus import *
+
 def line(x, y, dx, dy, length):
     """Uma linha de células de comprimento 'length' começando em (x, y) na direcção (dx, dy)."""
     return {(x + i * dx, y + i * dy) for i in range(length)}
@@ -38,3 +40,35 @@ def display(pacman,pastilha,obstaculos,path=[]):
             output += ch + " "
         output += "\n"
     print(output)
+    
+    
+    
+    def astar_search(problem, h=None):
+        """A* search is best-first graph search with f(n) = g(n)+h(n).
+        You need to specify the h function when you call astar_search, or
+        else in your Problem subclass."""
+        h = memoize(h or problem.h, 'h')
+    return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
+   
+def planear_online(pacman,pastilha,obstaculos):
+    print("MUNDO")
+    display(pacman,pastilha,obstaculos,path=[])
+    print("MODELO")
+    #funcao shenanigans
+    
+    print()
+    print()
+    
+    pass
+def planear_adapt_online(pacman,pastilha,obstaculos):
+    pass
+    
+
+pacman=(1,1)
+pastilha=(1,6)
+l = line(2,2,1,0,6)
+c = line(2,3,0,1,4)
+fronteira = quadro(0,0,10)
+obstaculos=fronteira | l | c
+print(fronteira)
+display(pacman,pastilha,obstaculos,path=[])
