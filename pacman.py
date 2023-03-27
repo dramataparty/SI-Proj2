30d93dd0c1071e3c4841c6c9829c526f788c0bd9
Dramata <diogobailaoforte@gmail.com>
2023-03-27 16:57:02 +0100

    significant progress

--- pacman.py
+++ pacman.py
@@ -2,33 +2,74 @@
 from searchPlus import *
 from p2_aux import *
 
+def astar_search2(problem, h=None):
+        """A* search is best-first graph search with f(n) = g(n)+h(n).
+        You need to specify the h function when you call astar_search, or
+        else in your Problem subclass."""
+        #h = memoize(h or problem.h, 'h')
+        f = lambda n: n.path_cost + h(n)
+        return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
+
+class pacproblem(Problem):
+
+    def __init__(self, initial, goal, obstacles):
+        self.initial=initial
+        self.goal=goal 
+        self.obstacles=set(obstacles) - {initial, goal}
+
+    directions = {"N":(0, -1), "W":(-1, 0), "E":(1,  0),"S":(0, +1)}  
+    
+    costs = {"N":1, "W":1, "E":1,"S":1}
+                  
+    def result(self, state, action): 
+        "Tanto as acções como os estados são representados por pares (x,y)."
+        (x,y) = state
+        (dx,dy) = self.directions[action]
+        return (x+dx,y+dy) 
+    
+    def actions(self, state):
+        """Podes move-te para uma célula em qualquer das direcções para uma casa 
+           que não seja obstáculo."""
+        x, y = state
+        return [act for act in self.directions.keys() if (x+self.directions[act][0],y+self.directions[act][1]) not in self.obstacles]
+    
+    def path_cost(self,c,state,action,new):
+        return c+self.costs[action]
+    
 
     
-def astar_search(problem, h=None):
-    """A* search is best-first graph search with f(n) = g(n)+h(n).
-    You need to specify the h function when you call astar_search, or
-    else in your Problem subclass."""
-    h = memoize(h or problem.h, 'h')
-    return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
-
 def planear_online(pacman,pastilha,obstaculos):
+    heurs = set()
+    expanded = []
     print("MUNDO")
     pworld = str(display(pacman,pastilha,obstaculos,path=[]))
-    rworld = pworld.split()
-    print(pworld)
     print("MODELO")
-    modelo = quadro[:pastilha[1]][:pastilha[0]]
+    obcopy =obstaculos.copy()
+    for i in obstaculos:
+        if manhatan(pacman,i) > 2:
+            obcopy.remove(i)    
+    modelo = str(display(pacman,pastilha,obcopy,path=[]))
     print(modelo)
     #funcao shenanigans
     nits = 0
-    print("ITERAÇÃO: " + str(nits))
-    print("_path__")
-    exp = 0 
-    print("Expandidos " + str(exp))
-    print("FIM: total de expandidos: "+ str(exp))
-    
-    
+    totexp = 0
+    '''while pacman != pastilha: 
+        path=[]
+        path.append(pacman)
+        pac2 = pacproblem(pacman,pastilha,obcopy,expanded,sol)
+        exp = 0
+        astar_search2(pacproblem(pac2,pac2.manhatan_goal))
+        nits += 1
+        print("ITERAÇÃO: " + str(nits))
+        print(str(display(pacman,pastilha,obcopy,path)))
+        Node.expand(pacproblem)
+        print(expanded)
+            
+        print("Expandidos " + str(exp))
+        totexp += exp
+    print("FIM: total de expandidos: "+ str(totexp))'''
     pass
+
 def planear_adapt_online(pacman,pastilha,obstaculos):
     print("MUNDO")
     display(pacman,pastilha,obstaculos,path=[])
@@ -36,7 +77,7 @@ def planear_adapt_online(pacman,pastilha,obstaculos):
     #funcao shenanigans
     nits = 0
     print("ITERAÇÃO: " + nits)
-    print(_path__)
+    print()
     exp = 0 
     print("Expandidos " + exp)
     print("FIM: total de expandidos: "+ exp)
@@ -50,6 +91,6 @@ pastilha=(1,6)
 l = line(2,2,1,0,6)
 c = line(2,3,0,1,4)
 fronteira = quadro(0,0,10)
-obstaculos=fronteira | l | c
+obstaculos= fronteira | l | c
 print(fronteira)
 planear_online(pacman,pastilha,obstaculos)
